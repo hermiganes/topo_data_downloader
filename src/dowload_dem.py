1c59f80c0bc082e0054f4311c785046b3a8831af
Miguel E <hermiganes@gmail.com>
2024-07-18 17:25:17 -0600

    Implementa ultimos cambios de feature

--- src/dowload_dem.py
+++ src/dowload_dem.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+import pandas as pd
 
 class INEGI_API:
     def __init__(self):
@@ -13,6 +13,7 @@ class INEGI_API:
             'Accept-Encoding': 'identity'
         }
 
+    
     def get_upc(self, clave):
         url = "https://www.inegi.org.mx/app/api/productos/interna_v2/componente/mapas/lista/resultados/"
         payload = {
@@ -41,62 +42,182 @@ class INEGI_API:
         }
         
         try:
-            response = requests.post(url, headers=self.headers, json=payload)
+            response = requests.post(url, headers=self.headers, json=payload)         
             if response.status_code == 200:
                 result = response.json()
                 return result
             else:
-                print(clave + "   Error en la solicitud. Código de estado:", response.status_code)
-                return None
+                raise Exception(f"Carta {clave} - Problema en la solicitud - Código de estado: {response.status_code}")
         except Exception as e:
-            print(clave + "   Error:", str(e))
-            return None
+            print( f"Error: {e}" )
 
-    def buscar_liga(self, json, tipo, ext):
-        liga = {"liga": ""}
-        if not json or not json['success']:
-            return None
-        
-        for mapa in json['list']['mapas']:
-            for formato in mapa["formatos"]:
-                texto = formato['url']['valor'].lower()
-                if tipo in texto and ext == 0 and "_b.zip" in texto:
-                    liga["liga"] = formato['url']['valor']
-                    return liga['liga']
-                elif tipo in texto and ext == 1 and "_as.zip" in texto:
-                    liga["liga"] = formato['url']['valor']
-                    return liga['liga']
-                elif tipo in texto and ext == 2 and "_gr.zip" in texto:
-                    liga["liga"] = formato['url']['valor']
-                    return liga['liga']
-                elif tipo in texto and ext == 3 and "_t.zip" in texto:
-                    liga["liga"] = formato['url']['valor']
-                    return liga['liga']
+    def buscar_liga(self, json, terreno=True, ext='ascii'):
+        """
+        Busca y devuelve un enlace de descarga para el primer DEM encontrado
+        con los parámetros tipo y ext dentro del objeto JSON proporcionado.
+
+        Args:
+        - json (dict): Objeto JSON que contiene información del DEM.
+        - terreno (Bool): Booleano para filtrar por tipo 'Terreno' (True) o 'Superficie' (False). 
+        - ext (str): Extensión ('ascii', 'bil', 'grid', 'tif') del archivo DEM a buscar .
+
+        Returns:
+        - str or None: Enlace de descarga del documento encontrado, o None si no se encontró coincidencia.
+
+        Example:
+        >>> INEGI_API.buscar_liga(json, True, 'tif')
+        """
+        try:
+            if json is not None:
                 
-        
-        return None
-
-    def download_dem(self, clave, tipo, ext, folder_output):
-
-        salida = self.get_upc(clave)
-        
-        if salida:
-            liga = self.buscar_liga(salida, tipo, ext)
-            if liga:
-                url = self.url_base + liga
-                try:
-                    response = requests.get(url, headers=self.headers)
-                    if response.status_code == 200:
-                        archivo_local = folder_output + clave + ".zip"
-                        with open(archivo_local, 'wb') as archivo:
-                            archivo.write(response.content)
-                        print(clave + "   descargado y guardado")
+                if json['success']:
+                    
+                    if terreno:
+                        tipo="terreno"
                     else:
-                        print(clave + "   Error en la solicitud. Código de estado:", response.status_code)
-                except Exception as e:
-                    print(clave + "   Error durante la descarga:", str(e))
-            else:
-                print(clave, "no tiene información")
-        else:
-            print(clave, "no se encontró en la base de datos")
+                        tipo="superficie"
+                    liga = {"liga": ""}
+                    for mapa in json['list']['mapas']:
+                        for formato in mapa["formatos"]:
+                            texto = formato['url']['valor'].lower()
+                            if tipo in texto and ext.lower() == 'bil' and "_b.zip" in texto:
+                                liga["liga"] = formato['url']['valor']
+                                return liga['liga']
+                            elif tipo in texto and ext.lower() == 'ascii' and "_as.zip" in texto:
+                                liga["liga"] = formato['url']['valor']
+                                return liga['liga']
+                            elif tipo in texto and ext.lower() == 'grid' and "_gr.zip" in texto:
+                                liga["liga"] = formato['url']['valor']
+                                return liga['liga']
+                            elif tipo in texto and ext.lower() == 'tif' and "_t.zip" in texto:
+                                liga["liga"] = formato['url']['valor']
+                                return liga['liga']   
+                    raise ValueError(f"no tiene el DEM solicitado")   
 
+                else:
+                    message = json['message'].replace("para el listado de cartas", "")
+                    #clave= json['list']['mapas'][0]['clave_carta']
+                    raise Exception(f" {message}")
+            else:
+                raise Exception(f"sin Json")
+            
+        except Exception as e:
+            print( f"Error: Carta {clave} - {e}" )
+
+
+
+    def download_dem(self, clave=None, terreno=True, ext='ascii', folder_output='output/', liga=None):
+        """
+        Descarga un DEM por su clave, tipo y extensión o por su url, en la carpeta especificada.
+
+        Args:
+        - clave (str): Clave única que identifica el documento a descargar.
+        - terreno (Bool): Booleano para filtrar por tipo 'Terreno' (True) o 'Superficie' (False). 
+        - ext (str): Extensión ('ascii', 'bil', 'grid', 'tif') del archivo DEM a buscar .
+        - folder_output (str): Ruta de la carpeta donde se guardarán los documentos descargados.
+        - liga (str): Liga de descargar del DEM, si de proporciona se usará en lugar buscar por clave. 
+
+        Returns:
+        - None
+
+        Example:
+        >>> INEGI_API.download_dem('e14b31d1', 'True, 'tif', 'output/')
+        """
+
+        try:
+            if clave is None and liga is None:
+                raise ValueError("Debe proporcionar  'clave' o 'liga' para descargar el DEM.")
+            if (liga is not None):
+                
+                url = self.url_base + liga
+                clave = f"{liga.split('/')[-1].split('_')[0]}" if clave is None else clave
+
+            else:
+                salida = self.get_upc(clave)        
+                liga = self.buscar_liga(salida, terreno, ext)
+                if liga:
+                    url = self.url_base + liga
+                else:
+                    return None        
+            response = requests.get(url, headers=self.headers)
+            if response.status_code == 200:
+                archivo_local = folder_output + clave + ".zip"
+                with open(archivo_local, 'wb') as archivo:
+                    archivo.write(response.content)
+                print(f"{clave} descargado y guardado")
+
+            else:
+                raise Exception(f"Problema en la solicitud o descarga - Código de estado: {response.status_code}")
+        except Exception as e:
+            print( f"Error: Carta {clave} - {e}" )
+    
+
+
+
+class ProcesadorCartas ():
+
+    def __init__(self, api, cartas):
+        self.api = api
+        self.cartas = cartas
+    
+    def crear_df(self):
+        """
+        Crea un DataFrame de todos los DEM disponibles para la lista de cartas (clave)
+
+        Returns:
+        - DataFrame con información de DEM disponibles
+        """
+        df_ = pd.DataFrame()
+        for clave in self.cartas:
+            respuesta = self.api.get_upc(clave)
+            df = pd.json_normalize(respuesta['list']['mapas'], record_path='formatos', meta=['key', 'titulo', 'entidad', 'url', 'edicion', 'escala', 'clave_carta', 'datum', 'iin', 'af'])
+            column_order = [
+                'key', 'titulo', 'entidad', 'url', 'edicion', 'af', 'escala', 'clave_carta', 'datum', 'iin', 
+                'upc', 'imagen', 'siglas', 'absoluto', 'dominio', 'folder', 'extension', 'datos_abiertos',
+                'clave', 'peso', 'origen', 'web', 'control', 'url.valor', 'url.target', 'url.estilo', 'adicional.estilo'
+            ]
+
+            df = df[column_order]
+            df_ = pd.concat([df_, df], ignore_index=True)
+            df_['tipo'] = "Terreno"
+            df_['tipo'] = df_['titulo'].apply(lambda x: 'Terreno' if 'terreno' in x.lower() else 'Superficie')
+            df_['clave_carta'] = df_['clave_carta'].str.lower()
+            df_["extension"] = df_["extension"].str.lower()
+            df_['id'] = df_['upc'].astype(str) + '_' + df_.index.astype(str)
+        return df_
+    
+    def filtrar_cartas(self, df=None, terreno=True, formatos=['ascii', 'bil', 'grid', 'tif'], year=None, quick=False):
+        """
+        Filtra y ordena los DEM según criterios especificados.
+
+        Args:
+        - df (DataFrame): DataFrame de cartas. Opcional.
+        - terreno (Bool): Booleano para filtrar por tipo 'Terreno' (True) o 'Superficie' (False).
+        - formatos (list): Lista de formatos de extensión en orden de preferencia, por default ['ascci', 'bil', 'grid', 'tif'] .
+        - year (Int): Año mínimo de edición, opcional.
+        - quick (Bool): Para regresar un DataFrame con los DEM más recientes de cada carta.
+
+        Returns:
+        - DataFrame filtrado, ordenado y agrupado con el DEM más reciente para cada carta.
+        """
+        if df is None :
+            df = self.crear_df()
+        
+        if terreno:
+            df = df[df['tipo'] == 'Terreno'].copy()
+        else:
+            df = df[df['tipo'] == 'Superficie'].copy()
+        
+        if year is not None:
+            df = df[df['edicion'] >= year].copy()
+        
+        if quick:
+            df = (df.sort_values(["edicion","peso"], ascending=[False,True])
+                  .groupby(['clave_carta','tipo']).head(1))
+        else:
+            df['formato_orden'] = (df['extension']
+                                   .apply(lambda x: formatos.index(x) if x in formatos else len(formatos)))
+            df = (df.sort_values(by=['formato_orden', 'edicion'], ascending=[True, False]) 
+                .groupby(['tipo', 'clave_carta']).head(1))
+
+        return df
